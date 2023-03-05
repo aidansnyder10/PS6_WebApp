29038a6b71df780f3673e8ba46029bd9c9c8b73e
aidansnyder10 <aidan10@uw.edu>
2023-03-04 16:41:58 -0800

    Final push

--- WebAppDocumentPS6/app.R
+++ WebAppDocumentPS6/app.R
@@ -1,51 +1,119 @@
-#
-# This is a Shiny web application. You can run the application by clicking
-# the 'Run App' button above.
-#
-# Find out more about building applications with Shiny here:
-#
-#    http://shiny.rstudio.com/
-#
-
 library(shiny)
+library(tidyverse)
+getwd()
 
-# Define UI for application that draws a histogram
+UAH <- read_delim("UAH-lower-troposphere-long.csv.bz2")
+
+view(UAH)
+
 ui <- fluidPage(
-
-    # Application title
-    titlePanel("Old Faithful Geyser Data"),
-
-    # Sidebar with a slider input for number of bins 
-    sidebarLayout(
-        sidebarPanel(
-            sliderInput("bins",
-                        "Number of bins:",
-                        min = 1,
-                        max = 50,
-                        value = 30)
+  tabsetPanel(type = "tab",
+              tabPanel("Opening Page",
+                       h1("UAH Lower Troposphere Data"),
+                       p("The University of Alabama Huntsville has been collecting data on the
+                          temperature of the", strong("lower troposphere"), "depending on month and region
+                          since 1978."), 
+                       p("This data contains", nrow(UAH), "observations with 4 variables." ),
+                       p("Below is a small random sample of the data."),
+                       dataTableOutput("small")
+                       ),
+              tabPanel("Plot Panel",
+                sidebarLayout(
+                  sidebarPanel( 
+                   
+                    fluidRow(h1("Select Month Below"),
+                             p("Lower Troposphere temperatures by year,
+                               which can be filtered by the month"),
+                      uiOutput("selectinput"),
+                      radioButtons("color", "choose color", 
+                                   choices = c("skyblue", "lawngreen", "orangered",
+                                                        "purple", "gold"))
+                    )
+                    ),
+                mainPanel(
+                  plotOutput("plot"),
+                  textOutput("monthchange")
+                )
+                )
+              ),
+              tabPanel("Table Panel",
+              sidebarLayout(
+                sidebarPanel(
+                  fluidRow(uiOutput("checkboxregion"))
+                  ),
+                mainPanel(
+                  textOutput("range_text"),
+                  tableOutput("table")
+                )
+              )
         ),
-
-        # Show a plot of the generated distribution
-        mainPanel(
-           plotOutput("distPlot")
-        )
-    )
+  
+ )
 )
+ 
 
-# Define server logic required to draw a histogram
 server <- function(input, output) {
-
-    output$distPlot <- renderPlot({
-        # generate bins based on input$bins from ui.R
-        x    <- faithful[, 2]
-        bins <- seq(min(x), max(x), length.out = input$bins + 1)
-
-        # draw the histogram with the specified number of bins
-        hist(x, breaks = bins, col = 'darkgray', border = 'white',
-             xlab = 'Waiting time to next eruption (in mins)',
-             main = 'Histogram of waiting times')
+  output$small <- renderDataTable({
+      UAH %>% 
+        sample_n(10)
     })
+  
+output$selectinput <- renderUI({
+  selectInput("select", "Choose:",
+              choices = unique(UAH$month))
+})
+  
+samplemon <- reactive({
+  UAH %>% 
+    filter(month %in% input$select)
+})
+
+  output$plot <- renderPlot({
+  samplemon() %>%
+      ggplot(aes(year, temp))+
+      geom_point(color = input$color) +
+      labs(x = "Year",
+           y = "Temperature in Celsius",
+           title = "Temperature of Lower Troposphere by Year")
+  })
+  
+  output$monthchange <- renderText({
+    paste("This scatterplot shows the Lower Troposphere temperatures 
+          over the years for month", input$select[1])
+  })
+  
+  output$table <- renderTable({
+    
+    sampletwo()
+
+  })
+  output$checkboxregion <- renderUI({
+    checkboxGroupInput("region", "Choose Regions",
+                       choices = unique(UAH$region))
+    
+  })
+  
+  sampletwo <- reactive({
+    UAH %>% 
+      filter(region %in% input$region) 
+  })
+
+range_text <- reactive({
+  selected <- input$region
+  filtered <- UAH %>% 
+    filter(region %in% input$region)
+  data_range <- range(filtered$temp)
+  paste0("Data Range:", data_range[1], "to", data_range[2])
+})
+  
+  
+  output$range_text <- renderText({
+    range_text()
+
+  })
 }
 
-# Run the application 
+
+
 shinyApp(ui = ui, server = server)
+
